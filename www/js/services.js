173fcda08283294a3021e9a13db1f48d172fcba4
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-23 10:56:02 -0600

    live preview of song and change tabs with initilized the song and resume the song when the tab is deselected

--- www/js/services.js
+++ www/js/services.js
@@ -18,3 +18,55 @@ angular.module('songhop.services', [])
 
     return o;
 })
+.factory('Recommendations', function($http, $q, SERVER){
+ var o = {
+    queue: []
+ };
+
+ var media;
+
+ o.getNextSong = function(){
+     return $http({
+         method: 'GET',
+         url: SERVER.url + '/recommendations'
+     }).success(function(data){
+        o.queue = o.queue.concat(data);
+     })
+ }
+
+ o.nextSong = function(){
+     o.queue.shift();
+
+     if(o.queue.length <= 3){
+         o.getNextSong();
+     }
+ }
+
+ o.playCurrentSong = function(){
+     var defer = $q.defer();
+
+      media = new Audio(o.queue[0].preview_url);
+
+      media.addEventListener("loadeddata", function(){
+          defer.resolve();
+      });
+
+      media.play();
+
+      return defer.promise;
+ }
+
+ o.haltAudio = function(){
+     if(media) media.pause();
+ }
+
+ o.init = function(){
+     if(o.queue.length === 0){
+         return o.getNextSong();
+     }else{
+         return o.playCurrentSong();
+     }
+ }
+
+ return o;
+})
