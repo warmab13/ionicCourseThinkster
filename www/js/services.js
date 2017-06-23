a17a39c30d66e543a7063d2fadc5174710bf6a09
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-23 09:22:32 -0600

    ionic course initial commit

--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,20 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', [])
+.factory('User', function(){
+    var o = {
+        favorites: [] 
+    }
+
+    o.addSongToFavorites = function(song){
+        if(!song) return false;
+
+        o.favorites.unshift(song);
+    }
+
+    o.removeSongFromFavorites = function(song, index){
+        if(!song) return false;
+
+        o.favorites.splice(index, 1);
+    }
+
+    return o;
+})
